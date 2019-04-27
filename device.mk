9e27ccb735bd68675d7b1012baaee0d78cc994ab
abhijiths0098 <abhijiths0098@gmail.com>
2019-04-27 16:16:59 +0000

    Adding Spectrum and fixing some stuff

--- device.mk
+++ device.mk
@@ -245,6 +245,8 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
+    init.spectrum.rc \
+    init.spectrum.sh \
     init.recovery.oneplus2.rc \
     ueventd.qcom.rc
 
@@ -315,5 +317,3 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/wifi/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
 
-# Inherit from oppo-common
-$(call inherit-product, device/oppo/common/common.mk)
